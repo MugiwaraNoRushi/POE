3f82fd318cedd601afea7a6f6e26b9bb678cb3eb
Jiraiya <shahrushi412@gmail.com>
2020-01-07 10:39:53 +0530

    faculty

--- Users/utils.py
+++ Users/utils.py
@@ -1,5 +1,12 @@
 import smtplib
 
+def Response(status,msg):
+    dict_resp = {
+        "status": status,
+        "message": msg
+    }
+    return dict_resp
+
 def sendEmail_reg_code(code,email):
     s = smtplib.SMTP('smtp.gmail.com', 587)
     s.starttls()
@@ -13,12 +20,21 @@ def sendEmail_reg_code(code,email):
     s.quit()
     pass
 
-def Response(status,msg):
-    dict_resp = {
-        "status": status,
-        "message": msg
-    }
-    return dict_resp
+def sendEmail_faculty(email):
+    s = smtplib.SMTP('smtp.gmail.com', 587)
+    s.starttls()
+    s.login("donotreply.poe@gmail.com", "Velocity@093")
+    text = """
+    Please contact the admin of our site to activate your account as a faculty.
+    It may two to three working hours.
+    Thank you for your patience.
+    """
+    message = 'Subject: {}\n\n{}'.format("Welcome to poe.com", text)
+
+    s.sendmail("donotreply.poe@gmail.com", email, message)
+    s.quit()
+    pass
+
 
 
 
